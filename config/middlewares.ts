9c1496bfeb681399ee1612328a2e6ec66315e930
Richard G <Felrichdev@gmail.com>
2025-11-30 00:45:14 -0500

    Se agrega img src en middleware en config

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -9,4 +9,33 @@ export default [
   'strapi::session',
   'strapi::favicon',
   'strapi::public',
+  {
+    name: 'strapi::security',
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          'connect-src': ["'self'", 'https:'],
+          // CRÍTICO: Añadir el dominio de R2 aquí (incluyendo el ID de tu cuenta)
+          'img-src': [
+            "'self'",
+            'data:',
+            'blob:',
+            'https://market-assets.strapi.io',
+            'https://02a7fbbf035bd49e589f5daebf915d18.r2.cloudflarestorage.com', // << TU ENDPOINT R2 >>
+            // Si también habilitaste el dominio público de desarrollo
+            // 'pub-XXXXXXXXXXXXXXXXXX.r2.dev', 
+          ],
+          'media-src': [
+            "'self'",
+            'data:',
+            'blob:',
+            'https://market-assets.strapi.io',
+            'https://02a7fbbf035bd49e589f5daebf915d18.r2.cloudflarestorage.com', // << TU ENDPOINT R2 >>
+          ],
+          upgradeInsecureRequests: null,
+        },
+      },
+    },
+  },
 ];
